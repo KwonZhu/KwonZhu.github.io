4f00984c9d0c7c6af370b469697928cf042d9495
kwonz <kwonzhu@gmail.com>
2021-12-10 21:41:32 +1030

    project structure 1.1

---
+++ app/Page/components/ResumePage/ResumePage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ResumePage = () => (
+
+);
+export default ResumePage;
---
+++ app/Page/components/ServicesPage/ServicesPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ServicesPage = () => (
+
+);
+export default ServicesPage;
