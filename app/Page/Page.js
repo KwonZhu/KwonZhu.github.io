7f6595d24b5db1acd966697cf0f66ccc5b16922f
kwonz <kwonzhu@gmail.com>
2021-12-16 23:55:30 +1030

    ResumePage.js build up

--- app/Page/Page.js
+++ app/Page/Page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from './components/HomePage';
-// import ResumePage from './components/ResumePage';
+import ResumePage from './components/ResumePage';
 // import ServicesPage from './components/ServicesPage';
 // import ContactPage from './components/ContactPage';
 
@@ -9,7 +9,7 @@ const Page = ({
 }) => (
   <div className="pages">
     { page === 'HOME' && (<HomePage />) }
-    {/* { page === 'RESUME' && (<ResumePage />) } */}
+    { page === 'RESUME' && (<ResumePage />) }
     {/* { page === 'SERVICES' && (<ServicesPage />) } */}
     {/* { page === 'CONTACT' && (<ContactPage />) } */}
   </div>
--- app/Page/components/ResumePage/ResumePage.js
+++ app/Page/components/ResumePage/ResumePage.js
@@ -1,6 +1,109 @@
-import React from 'react';
+import React from "react";
 
 const ResumePage = () => (
-
+  <div className="page">
+    <div className="page__header">
+      <h2 className="page__title">RESUME</h2>
+    </div>
+    <div className="page__content">
+      <div className="resumePage__sub">
+        <div>
+          <h3 className="resumeSub__title">Education</h3>
+          <div className="timelines">
+            <div className="timeline">
+              <h4 className="experience__title">Specialization Course</h4>
+              <div className="experience__meta">
+                <span className="experience__year">2010</span>
+                &nbsp;
+                <i className="experience__divider"></i>
+                &nbsp;
+                <span className="experience__company">Apple Inc.</span>
+              </div>
+              <div className="experience__description">
+                Mauris magna sapien, pharetra consectetur fringilla vitae,
+                interdum sed tortor.
+              </div>
+            </div>
+            <div className="timeline">
+              <h4 className="experience__title">Specialization Course</h4>
+              <div className="experience__meta">
+                <span className="experience__year">2010</span>
+                &nbsp;
+                <i className="experience__divider"></i>
+                &nbsp;
+                <span className="experience__company">Apple Inc.</span>
+              </div>
+              <div className="experience__description">
+                Mauris magna sapien, pharetra consectetur fringilla vitae,
+                interdum sed tortor.
+              </div>
+            </div>
+            <div className="timeline">
+              <h4 className="experience__title">Specialization Course</h4>
+              <div className="experience__meta">
+                <span className="experience__year">2010</span>
+                &nbsp;
+                <i className="experience__divider"></i>
+                &nbsp;
+                <span className="experience__company">Apple Inc.</span>
+              </div>
+              <div className="experience__description">
+                Mauris magna sapien, pharetra consectetur fringilla vitae,
+                interdum sed tortor.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3 className="resumeSub__title">Experience</h3>
+          <div className="timelines">
+            <div className="timeline">
+              <h4 className="experience__title">Specialization Course</h4>
+              <div className="experience__meta">
+                <span className="experience__year">2010</span>
+                &nbsp;
+                <i className="experience__divider"></i>
+                &nbsp;
+                <span className="experience__company">Apple Inc.</span>
+              </div>
+              <div className="experience__description">
+                Mauris magna sapien, pharetra consectetur fringilla vitae,
+                interdum sed tortor.
+              </div>
+            </div>
+            <div className="timeline">
+              <h4 className="experience__title">Specialization Course</h4>
+              <div className="experience__meta">
+                <span className="experience__year">2010</span>
+                &nbsp;
+                <i className="experience__divider"></i>
+                &nbsp;
+                <span className="experience__company">Apple Inc.</span>
+              </div>
+              <div className="experience__description">
+                Mauris magna sapien, pharetra consectetur fringilla vitae,
+                interdum sed tortor.
+              </div>
+            </div>
+            <div className="timeline">
+              <h4 className="experience__title">Specialization Course</h4>
+              <div className="experience__meta">
+                <span className="experience__year">2010</span>
+                &nbsp;
+                <i className="experience__divider"></i>
+                &nbsp;
+                <span className="experience__company">Apple Inc.</span>
+              </div>
+              <div className="experience__description">
+                Mauris magna sapien, pharetra consectetur fringilla vitae,
+                interdum sed tortor.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
+
 export default ResumePage;
