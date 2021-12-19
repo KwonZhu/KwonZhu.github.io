6b6b5524601964c531bf7e40b68aa7ca39c6a7d0
kwonz <kwonzhu@gmail.com>
2021-12-19 22:15:20 +1030

    ContactPage.js build up

--- app/Page/Page.js
+++ app/Page/Page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomePage from './components/HomePage';
 import ResumePage from './components/ResumePage';
 import ServicesPage from './components/ServicesPage';
-// import ContactPage from './components/ContactPage';
+import ContactPage from './components/ContactPage';
 
 const Page = ({
   page,
@@ -11,7 +11,7 @@ const Page = ({
     { page === 'HOME' && (<HomePage />) }
     { page === 'RESUME' && (<ResumePage />) }
     { page === 'SERVICES' && (<ServicesPage />) }
-    {/* { page === 'CONTACT' && (<ContactPage />) } */}
+    { page === 'CONTACT' && (<ContactPage />) }
   </div>
 );
 export default Page;
--- app/Page/components/ContactPage/ContactPage.js
+++ app/Page/components/ContactPage/ContactPage.js
@@ -1,7 +1,56 @@
 import React from 'react';
 
 const ContactPage = () => (
-
+  <div className="page">
+    <div className="page__header">
+      <h2 className="page__title">Contact</h2>
+    </div>
+    <div className="page__content">
+      <div className="contactPage__sub">
+        <div>
+          <h3 className="contactSub__title">Get in Touch</h3>
+          <div id="map"></div>
+          <div class="contact__details">
+            <div>
+              <span class="material-icons">place</span>
+              Adelaide, Australia
+            </div>
+            <div>
+              <span class="material-icons">email</span>
+              <a href="mailto:kwonzhu@gmail.com">kwonzhu@gmail.com</a>
+            </div>
+            <div>
+              <span class="material-icons">call</span>
+              +61 043 4837 886
+            </div>
+            <div>
+              <span class="material-icons">done</span>
+              Freelance Available
+            </div>
+          </div>
+        </div>
+        <div>
+          <h3 className="contactSub__title">Contact Form</h3>
+          {/* <!-- modify this form HTML and place wherever you want your form --> */}
+          <form action="https://formspree.io/f/xbjqlnoe" method="post" target="_blank">
+            <div>
+              <label for="name">Your name:</label>
+              <input type="name" placeholder="Full Name" required></input>
+            </div>
+            <div>
+              <label for="email">Your email:</label>
+              <input type="email" name="_replyto" placeholder="Email Address"required></input>
+            </div>
+            <div>
+              <label for="message">Your message:</label>
+              <textarea name="message" placeholder="Massage for Me" required rows="5"></textarea>
+            </div>
+            <input type="submit" name="submit" class="send" value="Send"></input>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default ContactPage;
